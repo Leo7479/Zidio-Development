a0d32b6508617333e3f27e0e524b7fcd36b46ea0
sharmila-03-k <sharmilakuntamukkala@gmail.com>
2025-05-13 19:32:07 +0530

    Accepted incoming changes and added application entity

--- code/src/main/java/com/zidioconnect/code/repositories/IStudentRepository.java
+++ code/src/main/java/com/zidioconnect/code/repositories/IStudentRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 public interface IStudentRepository extends JpaRepository<Student, Long> {
     Student findByUser_Id(Long userId);
     boolean existsByUser_Id(long userId);
-
 }
