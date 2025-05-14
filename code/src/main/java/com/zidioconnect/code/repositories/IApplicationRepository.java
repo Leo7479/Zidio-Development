cff0aa81e0a3c20a5bfa1bbca6e3edc8f2d18883
sharmila-03-k <sharmilakuntamukkala@gmail.com>
2025-05-14 17:09:37 +0530

    Add @Repository annotation to IApplicationRepository

--- code/src/main/java/com/zidioconnect/code/repositories/IApplicationRepository.java
+++ code/src/main/java/com/zidioconnect/code/repositories/IApplicationRepository.java
@@ -4,9 +4,11 @@ import com.zidioconnect.code.models.Application;
 import com.zidioconnect.code.models.Student;
 import com.zidioconnect.code.models.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IApplicationRepository extends JpaRepository<Application, Long> {
     List<Application> findByStudent(Student student);
     List<Application> findByJobPosting(JobPosting jobPosting);
